eba9b85a9185ca075735bbf470618e1899aa45a1
Zorrox0117 <83249731+Zorrox0117@users.noreply.github.com>
2021-06-06 17:28:41 -0600

    Menú =+ 1

--- Moon Ligth Redemption.py
+++ Moon Ligth Redemption.py
@@ -3,14 +3,17 @@ import pygame, sys
 pygame.init()
 #se inicia la ventana
 
-screen = pygame. display.set_mode([500,600])
+screen = pygame. display.set_mode([900,700])
 pygame.display.set_caption("Moon light Redemption")
 clock = pygame.time.Clock()
 
+# se configura el bg dentro de la pantalla de juego
 
-Font_tutulo = pygame.font.Font("8-BIT WONDER.TTF", 20)
+#bg = pygame.image.load("Imagenes/Background.jpg").convert()
+
+Font_tutulo = pygame.font.Font("8-BIT WONDER.TTF", 40)
 Titulo = Font_tutulo.render("Moon light Redemption", 0, (255, 255, 255))
-label_font = pygame.font.Font("8-BIT WONDER.TTF", 10)
+label_font = pygame.font.Font("8-BIT WONDER.TTF", 20)
 label_user = label_font.render("Write your name", 0, (255, 255, 255))
 
 def draw_text(txt, color, x, y, font = Font_tutulo):
@@ -18,16 +21,15 @@ def draw_text(txt, color, x, y, font = Font_tutulo):
     screen.blit(text_obj, (x - text_obj.get_width() // 2, y))
 
 def menu():
-    # se configura el bg dentro de la pantalla de juego
 
-    bg = pygame.image.load("Imagenes/Background.jpg").convert()
 
-    Textbox = pygame.Rect(270, 100, 125, 25)
-    button_Easy = pygame.Rect(190,190,120,45)
-    button_Medium = pygame.Rect(190,270,120,45)
-    button_Hard = pygame.Rect(190,350,120,45)
-    button_Instructions = pygame.Rect(260,500,120,45)
-    button_Credits = pygame.Rect(120,500,120,45)
+    Textbox = pygame.Rect(450+10,150, 300, 50)
+    button_play = pygame.Rect(450-300//2,450,300,50)
+    button_Easy = pygame.Rect(450-150//2-150-75,300,150,50)
+    button_Medium = pygame.Rect(450-150//2,300,150,50)
+    button_Hard = pygame.Rect(600,300,150,50)
+    button_Instructions = pygame.Rect(450-300-10,600,300,50)
+    button_Credits = pygame.Rect(450+10,600,300,50)
     clock = pygame.time.Clock()
 
     mouse_click = False
@@ -47,22 +49,25 @@ def menu():
             active = False
         if mouse_click:
             active = False
-        if button_Easy.collidepoint((cursor_x , cursor_y)):
+        if button_Easy.collidepoint((cursor_x, cursor_y)):
             if mouse_click:
                 print("Level Easy")
-        if button_Medium.collidepoint((cursor_x , cursor_y)):
+                level()
+        if button_Medium.collidepoint((cursor_x, cursor_y)):
             if mouse_click:
                 print("Level Madium")
-        if button_Hard.collidepoint((cursor_x , cursor_y)):
+                level()
+        if button_Hard.collidepoint((cursor_x, cursor_y)):
             if mouse_click:
                 print("Level Hard")
-        if button_Credits.collidepoint((cursor_x , cursor_y)):
+                level()
+        if button_Credits.collidepoint((cursor_x, cursor_y)):
             if mouse_click:
                 print("Credits")
-        if button_Instructions.collidepoint((cursor_x , cursor_y)):
+        if button_Instructions.collidepoint((cursor_x, cursor_y)):
             if mouse_click:
                 print("Instructions")
-        if Textbox.collidepoint((cursor_x , cursor_y)):
+        if Textbox.collidepoint((cursor_x, cursor_y)):
             if mouse_click:
                 active = True
         mouse_click = False
@@ -80,14 +85,17 @@ def menu():
                 if active == True:
                     if event.key == pygame.K_BACKSPACE:
                         User = User[:-1]
-                    elif len(User) < 15:
+                    elif len(User) < 10:
                         User += event.unicode
 
         #-----------------drwaw--------------------------#
-        screen.blit(bg, [-20, -200])
-        screen.blit(Titulo, (50, 50))
-        screen.blit(label_user, (90,105))
 
+       # screen.blit(bg, [-20, -200])
+        screen.fill((0,0,0))
+        screen.blit(Titulo, (50, 50))
+        screen.blit(label_user, (450-150//2-150-75,160))
+
+        pygame.draw.rect(screen,(255,0,0), button_play)
         pygame.draw.rect(screen, (255, 0, 0), button_Easy)
         pygame.draw.rect(screen, (255, 0, 0), button_Medium)
         pygame.draw.rect(screen, (255, 0, 0), button_Hard)
@@ -97,8 +105,65 @@ def menu():
             pygame.draw.rect(screen, (255, 255, 255), Textbox)
         else:
             pygame.draw.rect(screen, (255, 0, 0), Textbox)
-        draw_text(User, (0, 0, 0), 335, 100)
+
+            #Texto escrito por el usuario
+        draw_text(User, (0,0,0), 600,160 , label_font)
         pygame.display.update()
-        
+
+
+
+def level():
+
+
+    #------------Barra superior----------------#
+    BarraSuperior = pygame.Rect(0,0, 900, 50)
+    ButtonExit = pygame.Rect(450-130//2,5,130,40)
+
+    #-------------Barra de progreso-----------#
+    BarraProgreso = pygame.Rect(0,700-60, 900, 60)
+    #----------------texto-------------#
+    Fuente_complementaria = pygame.font.Font("LVDCGO__.TTF", 15)
+    LabelScore = Fuente_complementaria.render("SCORE:", 0, (0, 0, 0))
+    LabelTime = Fuente_complementaria.render("TIME:", 0, (0, 0, 0))
+    LabelLives = Fuente_complementaria.render("LIVES:", 0, (0, 0, 0))
+
+    level_1 = False
+    mouse_click = False
+
+    while not level_1:
+        cursor_x, cursor_y = pygame.mouse.get_pos()
+
+        #-------Volver a menu------#
+        if ButtonExit.collidepoint((cursor_x, cursor_y)):
+            if mouse_click:
+                print("Exit")
+                menu()
+        mouse_click = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    mouse_click = True
+
+
+       # screen.blit(bg, [-20, -200])
+
+        screen.fill((0,0,0))
+
+        #----------draw------------#
+
+        pygame.draw.rect(screen, (255, 255, 255), BarraSuperior)
+        pygame.draw.rect(screen, (255, 0, 0), ButtonExit)
+        pygame.draw.rect(screen, (255, 255, 255), BarraProgreso)
+        screen.blit(LabelScore, (450 - 150 // 2 - 150 - 75, 665))
+        screen.blit(LabelTime, (450 - 150 // 2, 665))
+        screen.blit(LabelLives, (600, 665))
+
+        pygame.display.update()
+
 
 menu()
+level()
